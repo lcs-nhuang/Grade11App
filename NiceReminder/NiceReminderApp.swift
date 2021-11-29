1e0dc8207ad05a7bc069ef1ce0ff6cba01d194fb
Nicole Huang <nhuang@lcs.on.ca>
2021-11-29 14:26:18 -0500

    Initial Commit

---
+++ NiceReminder/NiceReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceReminderApp.swift
+//  NiceReminder
+//
+//  Created by Nicole on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct NiceReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
